82867846fcfc4477a7a0d361792cbb9eb8f18ca0
mdaadoun <daadounmichael@gmail.com>
2023-11-23 11:14:46 +0100

    uncomment

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# from pybot import Robot
 from pybot import Robot
 robot = Robot()
 print(robot)
-
-# def hello():
-#     print("hello")
-
-
-# robot.ajouter_bouton("hello", hello)
-
-# robot.allumer_ecran()
-
-# robot.lancer_boucle()
